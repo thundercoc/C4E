b4ddf22002838afbc71f75a33c4bf71925afb6bd
trungquan <58217691+tquann@users.noreply.github.com>
2020-01-07 20:37:33 +0700

    Add files via upload

---
+++ iphoneProMax.js
@@ -0,0 +1,36 @@
+
+
+document.getElementById("p1").innerHTML = "Sau sự kiện ra mắt dòng iPhone 11, nhiều câu hỏi đặt ra rằng đâu là thiết bị đáng mua nhất? Theo cá nhân mình, iPhone 11 Pro là sản phẩm đáng lựa chọn nhất. Không chỉ vì thiết bị này được tích hợp đầy đủ các tính năng mới nhất của Apple mà còn sở hữu thiết kế nhỏ nhắn cùng giá 'mềm' hơn hẳn iPhone 11 Pro Max. Hãy cùng xem qua trang công nghệ uy tín Techradar đã đánh giá thế nào về iPhone 11 Pro nhé.";
+document.getElementById("p2").innerHTML = "Trong những năm qua, bên cạnh việc tung ra các phiên bản tiêu chuẩn (như iPhone 8, X,...) thì Apple còn công bố một vài phiên bản nâng cấp đi kèm (như iPhone 8 Plus, iPhone XS,..). Do đó, chẳng lấy làm lạ khi Apple tung ra iPhone 11 Pro đi kèm với iPhone 11.";
+document.getElementById("p3").innerHTML = "Mặc dù nhìn thoáng qua thì iPhone 11 Pro trông không khác gì hai thiết bị còn lại, tuy nhiên chiếc iPhone 11 Pro là phiên bản có thân hình nhỏ nhất và sở hữu đến 3 camera sau (trong khi iPhone 11 chỉ có 2 camera). Theo ý kiến cá nhân thì nếu bạn muốn sở hữu một chiếc iPhone 11 với gần như đầy đủ tính năng mới của iOS, chiếc iPhone 11 Pro sẽ là lựa chọn hoàn hảo."
+document.getElementById("p4").innerHTML = "Tuy nhiên, nếu nói rằng dòng iPhone 11 trông giống hệt dòng iPhone X thì thật không công bằng, nhất là khi phần thiết kế cụm camera sau đã được thay đổi hoàn toàn tạo nên sự khác biệt. So với dòng sản phẩm đời trước thì bộ ba thiết bị thuộc dòng iPhone 11 có cụm camera to hơn, nổi bật hơn nhiều."
+document.getElementById("p5").innerHTML = "Giống như iPhone 11, chiếc iPhone 11 phiên bản Pro cũng được trang bị màn hình 'tai thỏ' quen thuộc của dòng iPhone X. Đã có nhiều câu hỏi đặt ra rằng, tại sao Apple lại duy trì thiết kế của dòng sản phẩm đời trước trên sản phẩm mới?"
+document.getElementById("p6").innerHTML = "Đối với một vài ý kiến cho biết, xu hướng cải tiến thiết kế sẽ tạo ra sức hút đến người dùng và việc cầm trên tay một sản phẩm với thiết kế mới sẽ mang đến cảm giác mới lạ, thú vị. Nhưng có lẽ Apple nhận ra rằng, việc mang đến một thiết kế hoàn toàn mới sẽ đồng nghĩa với việc bạn phải chi một khoảng tiền không hề nhỏ để sở hữu chiếc máy."
+document.getElementById("p7").innerHTML = "Màn hình của iPhone 11 Pro có kích thước là 5.8 inch cùng độ phân giải là 2.436 x 1.125 pixels. Kết hợp cùng tấm nền Super Retina XDR hứa hẹn sẽ mang đến trải nghiệm tuyệt vời nhất có thể đến với người dùng iOS."
+document.getElementById("p8").innerHTML = "Như những gì mà chúng ta mong đợi, iPhone 11 Pro nói riêng và dòng iPhone 11 nói chung đã tự hào khi được chạy hệ điều hành iOS 13 - phiên bản mới nhất của hệ điều hành Apple. Phiên bản này mang lại các tính năng mới và độc đáo, mặc dù những tính năng này cũng sẽ được triển khai cho các mẫu iPhone đời trước (như dòng iPhone X) thông qua các bản cập nhật phần mềm."
+document.getElementById("p9").innerHTML = "Còn hiệu năng thì chắc khỏi phải bàn gì nhiều khi mà iPhone 11 Pro được trang bị con chip mạnh nhất ở thời điểm hiện tại - Apple A13. Cùng với đó, CPU của dòng iPhone 11 còn được đánh giá là nhanh nhất ở thời điểm hiện tại, vượt mặt cả Kirin 980 hay Snapdragon 855 của Qualcomm."
+document.getElementById("p10").innerHTML = "Nhờ đó mà trong một số lĩnh vực như chơi game, iPhone 11 Pro là một trong những sự lựa chọn vô cùng hoàn hảo. Hứa hẹn rằng con chip này sẽ không làm người dùng phải thất vọng."
+document.getElementById("p11").innerHTML = "Khác với iPhone 11 phiên bản tiêu chuẩn, chiếc iPhone 11 Pro sở hữu cụm 3 camera sau, được xếp thành hình tam giác và nằm gọn trong khung hình vuông. Theo nguồn tin cho hay thì thông số của 3 ống kính này gồm: Cảm biến chính 12 MP, cảm biến góc siêu rộng 12 MP cùng cảm biến tele 12 MP."
+document.getElementById("p12").innerHTML = "Nhìn chung thì so với các dòng sản phẩm đời trước, Apple đã bắt đầu áp dụng xu hướng cải tiến thiết kế và mang đến những kiêu dáng độc lạ thay vì khoác lên chiếc áo 'cũ xì'. Hi vọng rằng trong tương lai, các dòng iPhone tiếp theo sẽ mang trên mình bộ áo độc lạ và gây ấn tượng đến người tiêu dùng."
+document.getElementById("p13").innerHTML = "Còn bây giờ hãy cùng trải nghiệm chụp ảnh cùng iPhone 11 phiên bản Pro này. Theo Techradar thì chất lượng hình ảnh tổng thể sắc nét hơn bao giờ hết, nhất là khi chụp ảnh ở điều kiện đủ sáng máy cho ra những bức ảnh đẹp mắt với màu sắc sinh động cùng độ tương phản phù hợp."
+document.getElementById("p14").innerHTML = "Chụp ảnh trong điều kiện có ánh sáng yếu luôn là mối lo ngại đối với những nhà sản xuất smartphone, nhưng với iPhone 11 Pro thì dường như mọi thứ đang dần trở nên dễ dàng. Hiệu suất ánh sáng yếu cũng được tự động cải thiện đã mang đến những bức ảnh chất lượng và đẹp mắt."
+document.getElementById("p15").innerHTML = "Ngoài ra, Apple cũng đã thực hiện một số thay đổi đối với pin trong iPhone 11 Pro. Theo như Apple đã công bố thì chiếc máy có thời lượng pin dài hơn đến 4 giờ đồng hồ so với chiếc iPhone Xs, qua đó cho thấy hãng đã lắng nghe nhiều hơn nhu cầu đến từ người tiêu dùng."
+document.getElementById("p16").innerHTML = "Về giá bán thì hẳn chúng ta đều biết rõ rằng, iPhone luôn là dòng smartphone có mức giá 'đắc đỏ'. Do đó, iPhone 11 Pro cũng sở hữu mức giá khởi điểm khá cao là 999 USD cho phiên bản ROM 64 GB. Máy cũng sẽ có thêm 1 phiên bản với ROM 256 GB có mức giá khởi điểm là 1.149 USD và phiên bản ROM tối đa là 512GB có giá 1.349 USD."
+
+var comts = [];
+var userCmt = {
+    email: "",
+    comment : ''
+};
+
+
+function cmt(){
+    let newCmt = document.getElementById("comment").value;
+    document.getElementById("comment").value = '';
+
+    let newEml = document.getElementById("email").value;
+    document.getElementById("email").value = '';
+    userCmt["email"] = newEml;
+    userCmt["comment"] = newCmt;
+    comts.push(userCmt);
+}
