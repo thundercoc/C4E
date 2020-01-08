29f71d60a18ff568e265ed32573b56b38f74f7ea
vuqu4ngminh <57358430+vuqu4ngminh@users.noreply.github.com>
2020-01-08 16:29:05 +0700

    Add files via upload
    Thêm tính năng comment

--- comment.js
+++ comment.js
@@ -1,20 +1,57 @@
-// comment
+var names = [];
+var comments = [];
 
-// data
-var comts = [];
-var userCmt = {
-    email: "",
-    comment : ''
-};
+if(localStorage.getItem("names")){
+    names = JSON.parse(localStorage.getItem("names"));
+}
+
+if(localStorage.getItem("comments")){
+    comments = JSON.parse(localStorage.getItem("comments"));
+}
 
 // nhận comment vào data
+function displayCmt(){
+    let display = '';
+
+    for(let i = 0;i < names.length;i++){
+        const checkEml = names[i];
+        const checkCmt = comments[i];
+        display += `
+            <div class="card">
+                <div class="card-body">
+                    <div class="media">
+                        <img src="https://static-znews.zadn.vn/images/avatar_silkhouse.gif">
+                        <div class="media-body">
+                            <div class="card-body">
+                                <h5 class="card-title">${checkEml}</h5>
+                                <p class="card-text">${checkCmt}</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+    document.getElementById("comments").innerHTML = display;
+}
+
+displayCmt();
 function cmt(){
+    let newEml = document.getElementById("email").value;
+    document.getElementById("email").value = '';
+
     let newCmt = document.getElementById("comment").value;
     document.getElementById("comment").value = '';
 
-    let newEml = document.getElementById("email").value;
-    document.getElementById("email").value = '';
-    userCmt["email"] = newEml;
-    userCmt["comment"] = newCmt;
-    comts.push(userCmt);
+    if(newCmt === '' || newEml === ''){
+        alert("Vui lòng nhập đầy đủ thông tin");
+    } else {
+        names.push(newEml);
+        comments.push(newCmt);
+
+        localStorage.setItem("names",JSON.stringify(names));
+        localStorage.setItem("comments",JSON.stringify(comments));
+
+        displayCmt();
+    }
 }
